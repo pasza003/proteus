3d7d7a7d4a5be0ee63f1b98e64e311ffbcc22cde
Pap Szabolcs <h162647@stud.u-szeged.hu>
2025-05-03 19:18:10 +0200

    Show links in sidebar based on authentication

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Component, OnDestroy, inject, signal } from '@angular/core';
+import { Component, OnDestroy, Signal, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +8,8 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { ProteusUser } from '../../models/proteus-user';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -15,33 +18,40 @@ import { RouterLink } from '@angular/router';
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent implements OnDestroy {
-  protected readonly links = [
-    { label: 'Home', link: '/' },
-    { label: 'Login', link: '/login' },
-    { label: 'Register', link: '/register' },
-  ];
-
-  protected readonly adminLinks = [
-    { label: 'Organisation', link: '/organisation' },
-    { label: 'Curriculum', link: '/curriculum' },
-    { label: 'Subject', link: '/subject' },
-  ];
-
-  protected readonly isMobile = signal(true);
-
   private readonly _mobileQuery: MediaQueryList;
   private readonly _mobileQueryListener: () => void;
 
-  constructor() {
+  public readonly isMobile = signal(true);
+  public readonly user: Signal<ProteusUser | null>;
+
+  constructor(private readonly authService: AuthService) {
+    this.user = toSignal(this.authService.currentUser(), { initialValue: null });
+
     const media = inject(MediaMatcher);
 
-    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQuery = media.matchMedia('(max-width: 800px)');
     this.isMobile.set(this._mobileQuery.matches);
-    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = (): void => this.isMobile.set(this._mobileQuery.matches);
     this._mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
 
-  ngOnDestroy(): void {
+  public isAdmin(): boolean {
+    return this.user()?.role === 'ADMIN';
+  }
+
+  public isUser(): boolean {
+    return this.user()?.role === 'USER';
+  }
+
+  public isAuthenticated(): boolean {
+    return this.user() !== null;
+  }
+
+  public logout(): void {
+    this.authService.logout();
+  }
+
+  public ngOnDestroy(): void {
     this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
   }
 }
