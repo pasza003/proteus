7f349a134ac6166e8c904662822f1d39fd7c50b6
Pap Szabolcs <h162647@stud.u-szeged.hu>
2025-05-17 22:25:27 +0200

    Add home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatListModule, MatNavList } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProteusUser } from '../../shared/models/proteus-user';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatNavList, RouterLink, MatListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private readonly authService = inject(AuthService);
+
+  public readonly user: Signal<ProteusUser | null>;
+
+  constructor() {
+    const user$: Observable<ProteusUser | null> = this.authService.currentUser();
+    this.user = toSignal(user$, { initialValue: null });
+  }
+}
